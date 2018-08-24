e2b550fcc206bde19c3cca38234c4095ef5ec97f
BouakkezAnouar <anouar.bouakkez@gmail.com>
2018-08-24 20:08:01 +0100

    Communicating with several clients

--- index.js
+++ index.js
@@ -11,11 +11,20 @@ const server = http.createServer((req, res) => {
 const io = require("socket.io").listen(server);
 
 io.sockets.on("connection", socket => {
+  // detect new connection
   socket.emit("message", "you just connected");
-  socket.broadcast.emit("message", "there a new user connected");
 
+  //recieving username from the user
+  socket.on("username", username => {
+    //saving the username
+    socket.username = username;
+    //send a message to other users
+    socket.broadcast.emit("message", socket.username + " has just connected");
+  });
+
+  //when receiving a message from a user send it to the other users
   socket.on("message", message => {
-    socket.emit("message", "message from naw");
+    socket.broadcast.emit("message", socket.username + " says: " + message);
   });
 });
 
